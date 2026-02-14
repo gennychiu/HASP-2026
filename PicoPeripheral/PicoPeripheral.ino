6a760b4dc772dbc0be3ee9ae7e87123b7d01bb0e
Tala <vtalaie@live.com>
2026-02-14 09:17:20 -0800

    Update PicoPeripheral.ino
    Changes to use the Geiger counter class.

--- PicoPeripheral/PicoPeripheral.ino
+++ PicoPeripheral/PicoPeripheral.ino
@@ -1,64 +1,91 @@
 #include <Wire.h>
+#include <ArduinoQueue.h>
+#include "Geiger.h"
 
 constexpr uint8_t I2C_ADDR = 0x2C;
-constexpr int POT_PIN = 1;
 
-const byte LED_PIN = 22;
-const byte INTERRUPT_PIN = 21;
+//#define DEBUG_MODE    // Comment this line out for production code, uncomment it for testing
 
-volatile uint8_t potByte = 0;
+#define LED_PIN 22
+#define GEIGER_1_INTERRUPT_PIN 21
+#define GEIGER_2_INTERRUPT_PIN 20
+#define GEIGER_QUEUE_SIZE 100
+
+#define GEIGER_1_COUNT_REGISTER 1
+#define GEIGER_1_DATA_REGISTER  2
+
+#define GEIGER_2_COUNT_REGISTER 3
+#define GEIGER_2_DATA_REGISTER  4
 
 volatile uint8_t registerNumber = 0;
 
-volatile byte state = LOW;
-volatile int hitCount = 0;
+Geiger geiger_1(GEIGER_1_INTERRUPT_PIN, GEIGER_QUEUE_SIZE);
+Geiger geiger_2(GEIGER_2_INTERRUPT_PIN, GEIGER_QUEUE_SIZE);
 
 void handleInterrupt() {
-  state = !state;
-  hitCount++;
-  digitalWrite(LED_PIN, state);
-}
+  unsigned long m;
+  m = millis();
 
-uint8_t readPotByte() {
-  return 0x31;
+  if (!(sio_hw->gpio_in & (1 << GEIGER_1_INTERRUPT_PIN)))   // If the pin is low, we have an interrupt on that pin
+  {
+    if (geiger_1.enqueueItem(m) == false)
+    {
+      Serial.println("Guiger 1 Queue is full.");
+    }
+  }
+
+  if (!(sio_hw->gpio_in & (1 << GEIGER_2_INTERRUPT_PIN)))   // If the pin is low, we have an interrupt on that pin
+  {
+    if (geiger_2.enqueueItem(m) == false)
+    {
+      Serial.println("Guiger 2 Queue is full.");
+    }
+  }
 }
 
 void onI2CRequest() {
-  //uint8_t value = potByte;
-  //Wire.write(&value, 1);
-  String sensorData = "";
-  if (registerNumber == 1)
+  switch (registerNumber)
   {
-    //sensorData = "S1&T1:70&T2:90&T3:200&T4:300";
-    sensorData = String(hitCount);
-  }
-  else if (registerNumber ==2)
-  {
-    //sensorData = "S1&C1:170&C2:290&C3:200&C4:350";
-    sensorData = String(hitCount);
-  }
-  else
-  {
-    //sensorData = "S1&M1:7&M2:9&M3:20&M4:8";
-    sensorData = String(hitCount);
-  }
-  //int bufferLength = sensorData.length() + 1;
-  int bufferLength = sensorData.length();
+    case GEIGER_1_COUNT_REGISTER:
+      {
+        unsigned short s = geiger_1.getQueueCount();
+        byte byteArray[sizeof(unsigned short)];
+        memcpy(byteArray, &s, sizeof(unsigned short));
+        Wire.write(byteArray, 2);
+      }
+      break;
 
-  for (int i = 0; i < 32 - bufferLength; i++)
-  {
-    sensorData.concat(" ");
-  }
-  char charArray[32];
-  sensorData.toCharArray(charArray, 32);
-  Wire.write(charArray, 32);
+    case GEIGER_1_DATA_REGISTER:
+      {
+        unsigned long l = geiger_1.dequeueItem();
+        byte byteArray[sizeof(unsigned long)];
+        memcpy(byteArray, &l, sizeof(unsigned long));
+        Wire.write(byteArray, 4);
+      }
+      break;
 
-  hitCount = 0;
+    case GEIGER_2_COUNT_REGISTER:
+      {
+        unsigned short s = geiger_2.getQueueCount();
+        byte byteArray[sizeof(unsigned short)];
+        memcpy(byteArray, &s, sizeof(unsigned short));
+        Wire.write(byteArray, 2);
+      }
+      break;
+
+    case GEIGER_2_DATA_REGISTER:
+      {
+        unsigned long l = geiger_2.dequeueItem();
+        byte byteArray[sizeof(unsigned long)];
+        memcpy(byteArray, &l, sizeof(unsigned long));
+        Wire.write(byteArray, 4);
+      }
+      break;
+  }
 }
 
 void onI2CReceive(int numBytes) {
   while (Wire.available())
-    //(void)Wire.read();
     registerNumber = Wire.read();
 }
 
@@ -72,20 +99,73 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  pinMode(GEIGER_1_INTERRUPT_PIN, INPUT_PULLUP);
+  pinMode(GEIGER_2_INTERRUPT_PIN, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), handleInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(GEIGER_1_INTERRUPT_PIN), handleInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(GEIGER_2_INTERRUPT_PIN), handleInterrupt, FALLING);
 
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
 }
 
 void loop() {
-  potByte = readPotByte();
-  
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  //delay(100);                      // wait for a second
-  //digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  //delay(100);                      // wait for a second
+  delay(500);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                      // wait for a second
 
-  //digitalWrite(LED_PIN, state);
+  if ((geiger_1.getQueueCount() > 0) || (geiger_2.dequeueItem() > 0))
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+#ifdef DEBUG_MODE
+  int n1 = geiger_1.getQueueCount();
+  if (n1 != 0)
+  {
+    Serial.println("Geiger 1 Queue Data: ");
+
+    for (int i=0; i<n1; i++)
+    {
+      unsigned long l = geiger_1.dequeueItem();
+      Serial.println(l);
+      byte byteArray[sizeof(unsigned long)];
+      memcpy(byteArray, &l, sizeof(unsigned long));
+      for (size_t j = 0; j < sizeof(unsigned long); j++)
+      {
+        Serial.println("byte[" + String(j) + "]: 0x" + String(byteArray[j], HEX));
+      }
+    }
+  }
+  else
+  {
+    Serial.println("Geiger 1 Queue is empty.");
+  }
+
+  int n2 = geiger_2.getQueueCount();
+  if (n2 != 0)
+  {
+    Serial.println("Geiger 2 Queue Data: ");
+
+    for (int i=0; i<n2; i++)
+    {
+      unsigned long l = geiger_2.dequeueItem();
+      Serial.println(l);
+      byte byteArray[sizeof(unsigned long)];
+      memcpy(byteArray, &l, sizeof(unsigned long));
+      for (size_t j = 0; j < sizeof(unsigned long); j++)
+      {
+        Serial.println("byte[" + String(j) + "]: 0x" + String(byteArray[j], HEX));
+      }
+    }
+  }
+  else
+  {
+    Serial.println("Geiger 2 Queue is empty.");
+  }
+#endif
 }
