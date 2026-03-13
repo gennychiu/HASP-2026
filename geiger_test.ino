60d0187cac4001e3ef165f17aafcbad0efec5e42
peashooter63 <karennachiu@gmail.com>
2026-03-13 09:51:17 -0700

    Update geiger_test.ino
    Updated Version. 
    
    - Will be adding a looper that gives exact amount of items soon.

--- geiger_test.ino
+++ geiger_test.ino
@@ -1,35 +1,172 @@
-#include <DFRobot_Geiger.h>
-#if defined ESP32
-#define detect_pin D3
-#else
-#define detect_pin 26 // Must configure to appropriate rp2040 board pin; gpio 26 is read from pico
-#endif
+#include <Wire.h>
+#include <ArduinoQueue.h> 
+#include <string.h>
 
-// WILL NOT BE GPIO 26 USE ANOTHER PIN FOR MORE GEIGERS
-/*!
-   @brief Constructor
-   @param pin   External interrupt pin
-*/
-DFRobot_Geiger  geiger(detect_pin);
+// Com 4 (bme pico) Com 5 (geiger pico)
 
-void setup()
-{
+#define GEIGER_1_INTERRUPT_PIN 26
+//#define GEIGER_2_INTERRUPT_PIN NUMBER
+//#define GEIGER_3_INTERRUPT_PIN NUMBER
+//#define GEIGER_4_INTERRUPT_PIN NUMBER
+//#define GEIGER_5_INTERRUPT_PIN NUMBER
+
+// Size
+
+int count = 0;
+
+// Global char buffer
+//uint8_t bufferCount[1];
+//uint32_t bufferTime[4];
+
+const uint8_t i2cAddr2 = 0x70;
+volatile int geigerCount = 1;        // modified in each ISR
+volatile uint8_t registerNumber = 0; // modified in onReceive event
+int copyCount = 0;
+volatile unsigned long geigerTime;
+volatile bool countFlag = false;
+volatile bool timeFlag = false;
+
+// Define Queues  
+ArduinoQueue<int> countQueue1(100);
+ArduinoQueue<unsigned long> timeQueue1(100);
+
+ArduinoQueue<int> countQueue2(100);
+ArduinoQueue<unsigned long> timeQueue2(100);
+
+
+void setup() {
+
   Serial.begin(115200);
-  //Start counting, enable external interrupt
-  geiger.start();
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Enable interrupts and associate a GPIO pin with a ISR
+  pinMode(GEIGER_1_INTERRUPT_PIN, INPUT_PULLUP);
+  //pinMode(GEIGER_2_INTERRUPT_PIN, INPUT_PULLUP);
+  //pinMode(GEIGER_3_INTERRUPT_PIN, INPUT_PULLUP);
+  //pinMode(GEIGER_4_INTERRUPT_PIN, INPUT_PULLUP);
+  //pinMode(GEIGER_5_INTERRUPT_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(GEIGER_1_INTERRUPT_PIN),ISR_geiger1, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(GEIGER_2_INTERRUPT_PIN),ISR_geiger2, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(GEIGER_3_INTERRUPT_PIN),ISR_geiger3, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(GEIGER_4_INTERRUPT_PIN),ISR_geiger4, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(GEIGER_5_INTERRUPT_PIN),ISR_geiger5, FALLING);
+  // FOUND OUT ITS NOT ENQUEUEING. 
+
+
+  Wire1.setSDA(2);
+  Wire1.setSCL(3);
+  //Wire1.begin(i2cAddr1);
+  Wire1.begin(i2cAddr2);
+
+  Wire1.onReceive(onReceive);
+  Wire1.onRequest(I2CRequest);
 }
 
 void loop() {
-  //Start counting, enable external interrupt
-  //geiger.start();
-  delay(3000);
-  //Pause the count, turn off the external interrupt trigger, the CPM and radiation intensity values remain in the state before the pause
-  //geiger.pause();
-  //Get the current CPM, if it has been paused, the CPM is the last value before the pause
-  //Predict CPM by falling edge pulse within 3 seconds, the error is ±3CPM
-  Serial.println(geiger.getCPM());
-  //Get the current nSv/h, if it has been paused, nSv/h is the last value before the pause
-  Serial.println(geiger.getnSvh());
-  //Get the current μSv/h, if it has been paused, the μSv/h is the last value before the pause
-  Serial.println(geiger.getuSvh());
+
+  // Turn LED on and off 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+
+
+void ISR_geiger1() {
+  // Hardware disables interrupt per event
+  //geigerCount = 1;
+  geigerTime = millis();
+
+  // Test if ISR ran:
+  Serial.println("Geiger1 ISR running.");
+
+  // Check if Queues is not full 
+
+  if (!countQueue1.isFull()) {
+    countQueue1.enqueue(geigerCount);
+    Serial.println(geigerCount);
+     Serial.println("Enqueueing geiger Data");
+      
+  }
+  else { 
+    Serial.println("Not Enqueueing Geiger1 Data");
+    // Set a Flag 
+  }
+
+ if (!timeQueue1.isFull() ) {       // Will fix this
+  timeQueue1.enqueue(geigerTime);
+  Serial.println("Enqueueing Time1 Data");
+  
+  }
+  else {
+    Serial.println("Not Enqueueing Time1 Data");
+    // Set a flag
+  }
+}
+
+void ISR_geiger2() {
+  // Hardware disables interrupt per event
+  geigerCount = 1;
+  geigerTime = millis();
+  
+  // Check if Queue is not full 
+  if (!countQueue2.isFull()) {
+    countQueue2.enqueue(geigerCount);
+  }
+  else {
+    Serial.println("Not Enqueueing Geiger2 Data");
+  }
+}
+
+void I2CRequest() {
+
+    // Disable Interrupts
+    
+
+  if (registerNumber == 2) {
+
+    // Clear Counts
+    //geigerCount = 0;  
+
+    if (!countQueue1.isEmpty()) {
+      int count = countQueue1.dequeue();
+      Wire1.write((uint8_t*)(&count),4);          ///4 bytes per int
+    }
+    else {
+      Serial.println("Count Queue full");
+      Wire1.write((uint8_t*)(&count),4);         // Note: This line was added to stop crashing when master does not get an ACK byte from slave. 
+    }
+  }
+  
+    
+    if (registerNumber == 3) {
+      
+      if (!timeQueue1.isEmpty()) {
+        unsigned long time = timeQueue1.dequeue();  
+        Wire1.write((uint8_t*)(&time),4);           // 4 byte per unsigned
+      }
+      else {
+        Serial.println("Time Queue full");
+      }
+    }
+      // Turn off 
+    
+      // Checking Item Anount in Queues if working (TEST)
+      unsigned long timeCountNum = timeQueue1.item_count();
+      unsigned int queueCountNum = countQueue1.itemCount();
+      Serial.println("Count Queue Items:");
+      Serial.println(queueCountNum);
+      Serial.println("Count Time Items:");
+      Serial.println(timeCountNum);
+
+     // Enable interrupts
+    
+}
+
+void onReceive(int numBytes) {
+  while (Wire1.available()) {
+    registerNumber = Wire1.read();
+    // Pi Continues to request for data until pico sends empty flag 
+  }
 }
